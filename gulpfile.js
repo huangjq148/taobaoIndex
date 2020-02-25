8f9a6521570117c8c02091bcfb8e0b2f5412ba0f
。 <oncwnuG3K2C-tg1xHhfsirZJQZHE@git.weixin.qq.com>
2020-02-26 00:46:56 +0800

    初始化项目

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+/**
+ *   @Author huangjq
+ *   @createDate 2020/2/23
+ */
+const { watch, series } = require('gulp');
+var gulp = require("gulp");
+var scss = require("gulp-sass");
+
+function css() {
+	return gulp.src("sass/index.scss")
+		.pipe(scss())
+		.pipe(gulp.dest("css"))
+	// cb();
+}
+
+// 可以只关联一个任务
+// watch('css/sass/*.scss', css);
+
+exports.default = css;
+// 或者关联一个任务组合
+// watch('src/*.js', series(clean, javascript));
+
+// gulp.task("default",function(){
+// 	console.log("gulp启动成功")
+// })
+//
+// function scssCompiler(cb){
+// 	console.log("scss")
+// 	cb()
+// 	return gulp.src("css/sass/index.scss")
+// 		.pipe(scss())
+// 		.pipe(gulp.dest("css"))
+// }
+// gulp.task("scss", scssCompiler)
+//
+// gulp.watch("css/sass/*.scss",scssCompiler);
